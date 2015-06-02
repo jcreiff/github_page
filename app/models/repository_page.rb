2a59d72d6e45ea2826b50495323f9a917b190e5c
Joe Reiff <jcreiff@gmail.com>
2015-06-02 16:34:12 -0400

    add all repo list methods, pass mocking tests

--- app/models/repository_page.rb
+++ app/models/repository_page.rb
@@ -9,8 +9,31 @@ class RepositoryPage
     HTTParty.get("https://api.github.com/users/#{@username}/repos?access_token=#{ENV["GITHUB_KEY"]}")
   end
 
-  def show_user
-    @page[0]["owner"]["login"]
+  def user_name(num)
+    @page[num]["owner"]["login"]
   end
 
+  def repo_name(num)
+    @page[num]["name"]
+  end
+
+  def description(num)
+    @page[num]["description"]
+  end
+
+  def last_update(num)
+   @page[num]["pushed_at"]
+  end
+
+  def language(num)
+    @page[num]["language"]
+  end
+
+  def stargazer_count(num)
+    @page[num]["stargazers_count"]
+  end
+
+  def fork_count(num)
+    @page[num]["forks_count"]
+  end
 end
--- test/models/repository_page_test.rb
+++ test/models/repository_page_test.rb
@@ -1,8 +1,47 @@
+require './app/models/repository_page.rb'
+
+class RepositoryPage
+
+  def get_data
+    JSON.parse(File.open('./test/models/my_repo_list.json').read)
+  end
+
+end
+
 class RepositoryPageTest < ActiveSupport::TestCase
 
   def test_can_get_username
     joe = RepositoryPage.new("jcreiff")
-    assert_equal "jcreiff", joe.show_user
+    assert_equal "jcreiff", joe.show_user(0)
   end
 
+  def test_can_get_repo_name
+    joe = RepositoryPage.new("jcreiff")
+    assert_equal "battleship", joe.repo_name(0)
+  end
+
+  def test_can_get_description
+    joe = RepositoryPage.new("jcreiff")
+    assert_equal "battleship", joe.description(0)
+  end
+
+  def test_can_get_last_update
+    joe = RepositoryPage.new("jcreiff")
+    assert_equal "2015-05-26T21:01:03Z", joe.last_update(0)
+  end
+
+  def test_can_get_language
+    joe = RepositoryPage.new("jcreiff")
+    assert_equal "Ruby", joe.language(0)
+  end
+
+  def test_can_get_stargazers
+    joe = RepositoryPage.new("jcreiff")
+    assert_equal 0, joe.stargazer_count(0)
+  end
+
+  def test_can_get_forks
+    joe = RepositoryPage.new("jcreiff")
+    assert_equal 0, joe.fork_count(0)
+  end
 end
